071beaa6553215de6b4bbe19220f7142411f313e
Loai Alwan <118984533+Meliods23@users.noreply.github.com>
2025-03-19 11:28:55 +0100

    Implemented and tested "findById" and "findByCode" functions in "CityDaoImpl "

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,10 +1,26 @@
 package se.lexicon;
 
+import se.lexicon.dao.CityDaoImpl;
+import se.lexicon.model.City;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Represents the entry point of the application.
  */
 public class Main {
     public static void main(String[] args) {
         // TODO: Needs completion
+
+        CityDaoImpl cityDao = new CityDaoImpl();
+        cityDao.findById(4).ifPresent(city -> System.out.printf(city.toString()));
+
+
+        List<City> cities = cityDao.findByCode("AFG");
+        for (City c : cities){
+            System.out.println(c.toString());
+        }
+
     }
 }
--- src/main/java/se/lexicon/dao/CityDaoImpl.java
+++ src/main/java/se/lexicon/dao/CityDaoImpl.java
@@ -1,8 +1,114 @@
 package se.lexicon.dao;
 
+import se.lexicon.db.dbConnection;
+import se.lexicon.model.City;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Represents the implementation of CityDao for interacting with the 'city' table in the database.
  */
-public class CityDaoImpl {
+public class CityDaoImpl implements CityDao{
     // TODO: Needs completion
+
+    List<City> cities;
+
+    public CityDaoImpl() {
+        this.cities = new ArrayList<>();
+    }
+
+    @Override
+    public Optional<City> findById(int id) {
+
+        try (
+                Connection connection = dbConnection.getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM City WHERE id LIKE ?")
+                ){
+
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()){
+                if (resultSet.next()){
+                    // Gets the city info if the city exists
+                    int rsId = resultSet.getInt("ID");
+                    String rsName = resultSet.getString("Name");
+                    String rsCountryCode = resultSet.getString("CountryCode");
+                    String rsDistrict = resultSet.getString("District");
+                    int rsPopulation = resultSet.getInt("Population");
+
+
+                    City city = new City(rsId, rsName, rsCountryCode,rsDistrict, rsPopulation);
+                    return Optional.of(city);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database :" + e.getMessage());
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public List<City> findByCode(String code) {
+
+        try (
+                Connection connection = dbConnection.getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM City WHERE CountryCode LIKE ?")
+        ){
+
+
+            statement.setString(1, code);
+
+            List<City> found = new ArrayList<>();
+
+            try (ResultSet resultSet = statement.executeQuery()){
+                while (resultSet.next()){
+                    // Gets the city info if the city exists
+                    int rsId = resultSet.getInt("ID");
+                    String rsName = resultSet.getString("Name");
+                    String rsCountryCode = resultSet.getString("CountryCode");
+                    String rsDistrict = resultSet.getString("District");
+                    int rsPopulation = resultSet.getInt("Population");
+
+
+                    City city = new City(rsId, rsName, rsCountryCode,rsDistrict, rsPopulation);
+                    found.add(city);
+                }
+                return found;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database :" + e.getMessage());
+        }
+
+        // Returns null if no cities are found
+        return null;
+    }
+
+    @Override
+    public List<City> findByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<City> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public City save(City city) {
+        return null;
+    }
+
+    @Override
+    public void update(City city) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
 }
